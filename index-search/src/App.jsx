61e77b267d6bcb97573942cfad69cb1798ff1dc4
Yami-san <jeanmarcopedraza@gmail.com>
2024-02-27 00:20:11 -0500

    change of enviroment to vite and adding eslint config

---
+++ index-search/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export function App () {
+  return (
+    <section className='top-header'>
+      <img className='logo-app' src='https://unavatar.io/github/Yami-San' />
+    </section>
+  )
+}
+
+export default App
